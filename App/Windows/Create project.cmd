2e4ee5694bb78a52c1d12fa316b8bf1fd5d31cea
winer156win-sudo <winer156win@gmail.com>
2025-12-14 18:54:37 +0000

    windows bata 0.0.1

---
+++ App/Windows/Create project.cmd
@@ -0,0 +1,8 @@
+echo off
+title Creating project
+cls
+type nul > "project settings.IGM"
+mkdir "scenes\scene 1"
+type nul > "scenes\scene 1\scene objects.IGM"
+type nul > "scenes\scene 1\scene events.IGM"
+exit
